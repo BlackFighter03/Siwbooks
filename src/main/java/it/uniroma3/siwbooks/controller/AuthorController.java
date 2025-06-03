60d3d7c367f44d619f1bf3abff9dd5eec02c84e8
BlackFighter03 <alenat.scarpa@gmail.com>
2025-06-03 09:52:36 +0200

    Pulizia

--- src/main/java/it/uniroma3/siwbooks/controller/AuthorController.java
+++ src/main/java/it/uniroma3/siwbooks/controller/AuthorController.java
@@ -1,27 +1,15 @@
 package it.uniroma3.siwbooks.controller;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import it.uniroma3.siwbooks.model.Author;
-import it.uniroma3.siwbooks.model.ImageEntity;
 import it.uniroma3.siwbooks.model.User;
 import it.uniroma3.siwbooks.service.AuthorService;
-import it.uniroma3.siwbooks.service.BookService;
-import it.uniroma3.siwbooks.service.CredentialsService;
-import it.uniroma3.siwbooks.service.ImageEntityService;
 import it.uniroma3.siwbooks.service.UserService;
-import jakarta.validation.Valid;
 
 @Controller
 public class AuthorController {
--- src/main/java/it/uniroma3/siwbooks/controller/GlobalController.java
+++ src/main/java/it/uniroma3/siwbooks/controller/GlobalController.java
@@ -4,8 +4,10 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+@ControllerAdvice
 public class GlobalController {
 	@ModelAttribute("userDetails")
 	public UserDetails getUser() {
